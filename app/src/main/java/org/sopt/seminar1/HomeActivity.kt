4ca80c97641ea91eda04e6e86ec3826ecf6676d9
gaeun23 <0423g@naver.com>
2021-04-25 12:50:56 +0900

    2차 세미나 과제

--- app/src/main/java/org/sopt/seminar1/HomeActivity.kt
+++ app/src/main/java/org/sopt/seminar1/HomeActivity.kt
@@ -1,7 +1,9 @@
 package org.sopt.seminar1
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.seminar1.databinding.ActivityHomeBinding
 
@@ -16,7 +18,57 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initButtonClickEvent()
+        initRepoAdapter()
+
     }
+
+
+    private fun initButtonClickEvent(){
+        binding.homeMoreBtn.setOnClickListener{
+            Toast.makeText(this, "more버튼 클릭", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,UserInfoActivity::class.java))
+        }
+    }
+
+    private fun initRepoAdapter(){
+        val repositoryListAdapter = RepositoryListAdapter()
+        binding.repositoryRecyclerList.adapter = repositoryListAdapter
+        repositoryListAdapter.repositoryList.addAll(
+            listOf<RepositoryInfo>(
+                    RepositoryInfo(
+                            "첫 번째 레파지토리",
+                            "설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다",
+                            "java"
+                    ),
+                    RepositoryInfo(
+                             "두 번째 레파지토리 두번째 레파지토리 두번째 레파지토리 두번째 레파지토리",
+                            "정보",
+                            "kt"
+                    ),
+                    RepositoryInfo(
+                            "세 번째 레파지토리",
+                            "설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다",
+                            "java"
+                    ),
+                    RepositoryInfo(
+                            "네 번째 레파지토리",
+                            "설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다",
+                            "java"
+                    ),
+                    RepositoryInfo(
+                            "마지막 레파지토리",
+                            "설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다설명이 길어지면 말 줄임표를 사용합니다",
+                            "java"
+                    )
+
+
+
+            )
+        )
+    }
+
+
 
 
 
